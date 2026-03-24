c3540992d56024c5fb2f0b8eafc0a92064389b8b
nilrey inter arm <nilrey@gmail.com>
2026-03-24 13:32:09 +0300

    read Config params

--- scripts/dataset_set_filter.py
+++ scripts/dataset_set_filter.py
@@ -8,19 +8,28 @@ from imagecorruptions import corrupt
 from datetime import datetime
 import json
 
-# список допустимых искажений, 
+# Параметры ClearML задачи 
+TASK_NAME = "Dataset Filter"
+TASK_PROJECT = "Initial Scripts"
+OUT_DATASET_PROJECT_NAME = "MIRROR"
+REQUIREMENTS_FILE = "requirements.txt"
+
+# Наложение фильтров
+# список допустимых искажений
 ALLOWED_CORRUPTION_NAMES = [
     "gaussian_noise", "zoom_blur", "jpeg_compression", "brightness", "saturate",
     "spatter", "speckle_noise", "impulse_noise", "shot_noise", "defocus_blur",
     "motion_blur", "contrast"
 ]
+# default values
+DEFAULT_SEVERITY = 5
+DEFAULT_CORRUPTION_NAME = "gaussian_noise"
 
-# директория для временных файлов
+# Paths
 # BUFFER_DIR = '/home/sadmin/Documents/Mirror/data/output'
 # BUFFER_DIR = '/home/ubuntu/mirror_storage/storage/data/output'
 BUFFER_DIR = 'D:/Tmp/Mirror/data/output'
-OUTPUT_DATASETS_NAME = "MIRROR"
-REQUIREMENTS_FILE = "requirements.txt"
+
 
 
 def add_filter(input_path=None, output_path=None, corruption_name="gaussian_noise", severity=5):
@@ -66,7 +75,6 @@ def add_filter(input_path=None, output_path=None, corruption_name="gaussian_nois
 
     cap.release()
     out.release()
-    cv2.destroyAllWindows()
     return frame_count
 
 
@@ -78,7 +86,7 @@ def get_parameter_from_task(task, param_name, default_value=None):
     try:
         param_value = task.get_parameter(f"General/{param_name}")
         if param_value is not None:
-            print(f"Используем параметр {param_name} из Task: {param_value}")
+            print(f"Param: {param_name} from Task: {param_value}")
             return param_value
     except Exception:
         pass
@@ -95,25 +103,30 @@ def save_task_parameters(task, hyperparams):
         if param_value is not None:
             task.set_parameter(f"General/{param_name}", str(param_value))
 
-    # Сохраняем input_dataset как строку в формате JSON
-    input_dataset = json.dumps({
-        "id": hyperparams.get("input_dataset_id"),
-        "location": Path(__file__).name
-    }, ensure_ascii=False)
-    algorithm = json.dumps({
-        "name": hyperparams.get("task_name", "Dataset Filter"),
-        "id": ""
-    }, ensure_ascii=False)
-    input_params = json.dumps({
-        "severity": hyperparams.get("severity", 5)
-    }, ensure_ascii=False)
-    task.set_configuration_object("input_dataset", input_dataset)
-    task.set_configuration_object("algorithm", algorithm)
-    task.set_configuration_object("input_params", input_params)
-    
-# input_dataset {"id":INPUT_DATASET_ID,"name":OUT_DATASET_NAME}
-# algorithm {"id":"eab875078f0f4d3b985388319ff910b7", "name": "Dataset Filter","location":"dataset_set_filter.py"}
-# input_params {"severity":SEVERITY}
+    """
+    Отладочный блок. 
+    Сделана имитация наличия конфигурационных объектов. При инициализации задачи, конфигурационных объектов нет. 
+    Сохраняем input_dataset как строку в формате JSON
+    """
+    # input_dataset = json.dumps({"id": None, "name": None, "location": None}, ensure_ascii=False)
+    # algorithm = json.dumps({ "name": None, "id": None }, ensure_ascii=False)
+    # input_params = json.dumps({ "corruption_name": None, "severity": None }, ensure_ascii=False)
+    # task.set_configuration_object("input_dataset", input_dataset)
+    # task.set_configuration_object("algorithm", algorithm)
+    # task.set_configuration_object("input_params", input_params)
+
+
+def get_config_values(task, config_name):
+    config_string = task.get_configuration_object(config_name)
+    if config_string:
+        try:
+            config_params = json.loads(config_string)
+            return config_params
+        except json.JSONDecodeError:
+            raise ValueError(f"json.loads вызвал ошибку конфигурационного объекта: {config_name}")
+    return None
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--project-name", required=False, help="Project name (optional if set in Task)")
@@ -129,44 +142,54 @@ def main():
     # Инициализируем задачу ClearML
     if has_cli_args:
         # Если есть аргументы, используем их для создания задачи
+        print("Args received. Create new task")
         task = Task.init(
             project_name=args.project_name,
-            task_name=args.task_name,
-            auto_connect_arg_parser=False,
-            auto_connect_frameworks=False
+            task_name=args.task_name
         )
         # Указываем зависимости из requirements.txt
         # task.add_requirements(REQUIREMENTS_FILE)
     else:
+        print("No args, use existed task")
         # Если нет аргументов, берем существующую задачу или создаем с временными именами
         task = Task.current_task()
         if task is None:
+            print("Noexisted task, create new")
             # Создаем задачу с временными именами, которые потом будут переопределены параметрами
             task = Task.init(
                 project_name="Temporary Project",
-                task_name="Temporary Task",
-                auto_connect_arg_parser=False,
-                auto_connect_frameworks=False
+                task_name="Temporary Task"
             )
             # Указываем зависимости из requirements.txt
             # task.add_requirements(REQUIREMENTS_FILE)
     
-    # Получаем параметры с приоритетом: Task параметры > аргументы командной строки > значения по умолчанию
-    project_name = get_parameter_from_task(task, "project_name") or args.project_name or "Initial Scripts"
-    task_name = get_parameter_from_task(task, "task_name") or args.task_name or "Dataset Filter"
-    input_dataset_id = get_parameter_from_task(task, "input_dataset_id") or args.input_dataset_id
-    corruption_name = get_parameter_from_task(task, "corruption_name") or args.corruption_name or "gaussian_noise"
-    severity = get_parameter_from_task(task, "severity") or args.severity or 2
+    # Извлекаем и выводим значения конфигурации
+    input_dataset_cfg = get_config_values(task, "input_dataset")
+    # algorithm_cfg = get_config_values(task, "algorithm")
+    input_params_cfg = get_config_values(task, "input_params")
+
+    if not input_dataset_cfg:
+        input_dataset_cfg = {"id": None, "name": None}
+    if not input_params_cfg:
+        input_params_cfg = {"severity": None, "corruption_name": None}
+
+    # Получаем параметры с приоритетом: Task config параметры > Task General параметры > аргументы командной строки > значения по умолчанию
+    project_name = get_parameter_from_task(task, "project_name") or args.project_name or TASK_PROJECT
+    task_name = get_parameter_from_task(task, "task_name") or args.task_name or TASK_NAME
+    input_dataset_id = input_dataset_cfg.get("id") or get_parameter_from_task(task, "input_dataset_id") or args.input_dataset_id
+    corruption_name = input_params_cfg.get("corruption_name") or get_parameter_from_task(task, "corruption_name") or args.corruption_name or DEFAULT_CORRUPTION_NAME
+    severity = input_params_cfg.get("severity") or get_parameter_from_task(task, "severity") or args.severity or DEFAULT_SEVERITY
+    out_dataset_name = input_dataset_cfg.get("name") or f"Out_Dataset_" + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
     # Проверяем только input_dataset_id, так как он критичен
     if not input_dataset_id:
         raise ValueError("input_dataset_id must be provided either as Task parameter or command line argument")
     
     # Преобразуем severity в int
-    try:
-        severity = int(severity)
-    except (ValueError, TypeError):
-        raise ValueError(f"severity должно быть числом, получено: {severity}")
+    # try:
+    #     severity = int(severity)
+    # except (ValueError, TypeError):
+    #     raise ValueError(f"severity must be a numeric: {severity}")
     
     # Сохраняем параметры в Task для будущих запусков
     hyperparams = {
@@ -183,7 +206,7 @@ def main():
         task.set_name(task_name)
         # Примечание: project нельзя изменить после создания, но для будущих запусков сохраним параметр
     
-    print(f"Параметры запуска:")
+    print(f"Task params:")
     print(f"Project name: {project_name}")
     print(f"Task name: {task_name}")
     print(f"Task ID: {task.id}")
@@ -196,7 +219,7 @@ def main():
 
     input_files = [p for p in input_root.iterdir() if p.is_file() ]
     if not input_files:
-        raise RuntimeError("Нет видеофайлов в input dataset")
+        raise RuntimeError("No files in input dataset")
 
     # создаем buffer_root если он не существует
     buffer_root = Path(BUFFER_DIR)
@@ -209,23 +232,19 @@ def main():
         if frame_count == 0:
             raise RuntimeError(f"Файл не создан или пуст: {out_file}")
     
-    ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    out_dataset_name = f"Out_{input_dataset.name}_{ts}"
-    print(f"Out Dataset name: {out_dataset_name}")
-    
-    dataset = Dataset.create(dataset_project=OUTPUT_DATASETS_NAME, dataset_name=out_dataset_name)
+    dataset = Dataset.create(dataset_project=OUT_DATASET_PROJECT_NAME, dataset_name=out_dataset_name)
     for f in sorted(buffer_root.iterdir()):
         if f.is_file():
             dataset.add_files(path=str(f))
     dataset.upload()
     dataset.finalize()
     if dataset.id:
-        print("Датасет успешно создан", dataset.id)
+        print("Dataset created success", dataset.id)
         for f in buffer_root.iterdir():
             if f.is_file():
                 f.unlink()
     else:
-        print(f"Ошибка при создании датасета dataset.id={dataset.id}")
+        print(f"Error on dataset creation, dataset id is empty: {dataset.id}")
 
 if __name__ == '__main__':
     main()
